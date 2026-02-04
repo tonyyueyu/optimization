32f5cbf6ba848024177796fc7a585d92dfa4ceb8
ShrishGoel <shrishgoel@gmail.com>
2026-02-03 22:19:34 -0800

    Fixed worker file

--- solver/src/worker.js
+++ solver/src/worker.js
@@ -1,15 +1,19 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const BACKEND_URL = "https://backend-service-696616516071.us-west1.run.app";
 
     if (url.pathname.startsWith('/api/')) {
-      return fetch(request);
+      const newUrl = new URL(url.pathname + url.search, BACKEND_URL);
+      
+      const newRequest = new Request(newUrl, request);
+      
+      return fetch(newRequest);
     }
 
     let response = await env.ASSETS.fetch(request);
-
     if (response.status === 404) {
-      response = await env.ASSETS.fetch(new URL('/index.html', request.url));
+      return env.ASSETS.fetch(new URL('/index.html', request.url));
     }
 
     return response;
