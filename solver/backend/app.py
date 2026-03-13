05e77d98614241cf90f8daba1825dd5134c67cd2
ShrishGoel <shrishgoel@gmail.com>
2026-03-12 23:45:02 -0700

    Removed gcs backup + prod changes

--- solver/backend/app.py
+++ solver/backend/app.py
@@ -58,7 +58,7 @@ STORAGE_LIMIT_BYTES = 1.5 * 1024 * 1024 * 1024  # 1.5 GB
 isCloud = True
 if isCloud:
     # EXECUTOR_HOST = "https://executor-service-696616516071.us-west1.run.app"
-    EXECUTOR_HOST = "https://executor-service-dev-696616516071.us-west1.run.app"
+    EXECUTOR_HOST = "https://executor-service-test-696616516071.us-west1.run.app/"
 
 else:
     EXECUTOR_HOST = "http://executor:8000"
